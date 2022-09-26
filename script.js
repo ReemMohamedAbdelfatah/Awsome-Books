5fe9148f372c28cc20d858e8826070e95b488cb4
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-09-26 15:31:18 +0200

    Add html structure

---
+++ script.js
@@ -0,0 +1,12 @@
+const AddButton = document.querySelector('#add-btn');
+
+class Book {
+constructor() {}
+}
+
+const bookList = [];
+const addBook = () => {
+
+};
+
+AddButton.addEventListener('click', addBook());
