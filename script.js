b46dd181a10532ce72bfbcefcc8a68ef83e50f15
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-09-27 09:20:13 +0200

    Add javascript

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const AddButton = document.querySelector('#add-btn');
-
-class Book {
-constructor() {}
+/*class Book {
+constructor(author,title) {
+  this.author = author;
+  this.title = title;
 }
-
-const bookList = [];
-const addBook = () => {
-
+addBook = (book) => {
+bookList.push(book);
 };
 
-AddButton.addEventListener('click', addBook());
+}*/
+
+const AddButton = document.querySelector('#addBtn');
+const bookTitle = document.querySelector('#title').value;
+const bookAuthor = document.querySelector('#author').value;
+const book = {author: bookAuthor, title: bookTitle};
+const bookList = [];
+console.log(bookList);
+console.log('Helllllllllo');
+const addBook = (e) => {
+  if(bookTitle != "" && bookAuthor != "") {
+    e.preventDefault();
+  alert('hi');
+  console.log(bookTitle);
+  bookList.push(book);
+  console.log(bookList);
+}
+else{
+  console.log("Value is empty");
+}
+
+}
+
+AddButton.addEventListener('click', addBook);
