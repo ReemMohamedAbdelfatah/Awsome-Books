8223fb378b0786a33bd0f4de2c235b5af0f406d2
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-10-04 08:56:53 +0200

    Add ES6 Module structure

--- singlePageScript.js
+++ singlePageScript.js
@@ -8,14 +8,23 @@ booklistLink.addEventListener('click', () => {
   booklistSection.style.display = 'flex';
   formSection.style.display = 'none';
   contactinfoSection.style.display = 'none';
+  booklistLink.classList.add('active');
+  formLink.classList.remove('active');
+  contactinfoLink.classList.remove('active');
 });
 formLink.addEventListener('click', () => {
   booklistSection.style.display = 'none';
   formSection.style.display = 'flex';
   contactinfoSection.style.display = 'none';
+  formLink.classList.add('active');
+  booklistLink.classList.remove('active');
+  contactinfoLink.classList.remove('active');
 });
 contactinfoLink.addEventListener('click', () => {
   booklistSection.style.display = 'none';
   formSection.style.display = 'none';
   contactinfoSection.style.display = 'block';
+  contactinfoLink.classList.add('active');
+  formLink.classList.remove('active');
+  booklistLink.classList.remove('active');
 });
